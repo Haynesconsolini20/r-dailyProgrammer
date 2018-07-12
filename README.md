36d09004b8827b15f5b34e93254ff49d5fdf9b4b
Sam Haynes <haynesconsolini@gmail.com>
2018-07-12 09:26:52 -0600

    Completed 365e in python3

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # r-dailyProgrammer
 A series of completed challenges from r/dailyProgrammer
+
+# EASY CHALLENGES:
+365: Knuth Up-Arrow Notation
+* Python3
+
---
+++ upArrowJava.py
@@ -0,0 +1,48 @@
+import unittest
+
+# CHALLENGE 365
+# DIFFICULTY: EASY
+# KNUTH UP ARROW NOTATION
+# Author: Sam Haynes
+# Date: 07/12/2018
+
+# This was a tough one, and it took me much longer than I'd like to admit. But a little bit of teamwork
+# with my roommate and a good night's sleep made the answer very clear the next morning. The difficult part
+# with this one was figuring out how to persist the result value through multiple computations. The key
+# turned out to be using it as the exponent, even in the base power case.
+
+
+def d(b, l, n):
+  if l == 1:
+    return b ** n
+  else:
+    result = b
+    for _ in range(1, n):
+      result = d(b, l - 1, result)
+    return result
+
+class knuthTest(unittest.TestCase):
+  def test_oneBang(self):
+    self.assertEqual(d(2, 1, 4), 16)
+  
+  def test_twoBangA(self):
+    self.assertEqual(d(2,2,3), 16)
+
+  def test_twoBangB(self):
+    self.assertEqual(d(2,2,4), 65536)
+
+  def test_threeBang(self):
+    self.assertEqual(d(2,3,3), 65536)
+
+  def test_challenge1(self):
+    self.assertEqual(d(1, 1, 0), 1)
+
+  def test_challenge2(self):
+    self.assertEqual(d(1,2,0), 1)
+
+  def test_challenge3(self):
+    self.assertEqual(d(-1, 3, 3), -1)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(knuthTest)
+testResult = unittest.TextTestRunner(verbosity=2).run(suite)
